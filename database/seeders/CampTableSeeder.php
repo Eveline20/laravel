629d3f7883227154ac82fec21c99e9d16637d15b
Eveline Kurnia <eveline.422020012@civitas.ukrida.ac.id>
2022-04-16 21:32:16 +0700

    seeders

--- database/seeders/CampTableSeeder.php
+++ database/seeders/CampTableSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Camp;
 
+
 class CampTableSeeder extends Seeder
 {
     /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -16,7 +17,9 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call([
-            CampTableSeeder::class
+            CampTableSeeder::class,
+            CampBenefitTableSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }
