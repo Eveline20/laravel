da6db8b16fcffcc606ce76030d255e0053d238e6
eveline20 <eveline.422020012@civitas.ukrida.ac.id>
2022-04-21 22:20:17 +0700

    middleware dashboard, integration admin dashboard

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -34,7 +34,7 @@ class CheckoutController extends Controller
        
         if($camp->isRegistered) {
             $request->session()->flash('error',"You already registered on {$camp->title} camp.");
-                return redirect(route('dashboard'));
+                return redirect(route('user.dashboard'));
         }
         return view('checkout.create', [
             'camp' => $camp
@@ -125,9 +125,5 @@ public function success()
 {
     return view('checkout.success');
 }
-public function invoice(Checkout $checkout)
-{
-    return $checkout;
 
-}
 }
