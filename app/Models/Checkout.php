cbe27c693c5ccef6f52508fb1f8e2bf3b5711bca
eveline20 <eveline.422020012@civitas.ukrida.ac.id>
2022-04-25 15:44:16 +0700

    feat: discount management admin
    revisi dan diskon manage

--- app/Models/Checkout.php
+++ app/Models/Checkout.php
@@ -11,20 +11,30 @@ class Checkout extends Model
 {
     use HasFactory, SoftDeletes;
 
-    protected $fillable = ['user_id', 'camp_id', 'card_number','expired','cvc','is_paid'];
+    protected $fillable = ['user_id', 'camp_id', 'payment_status', 'midtrans_url', 'midtrans_booking_code'];
 
     public function setExpiredAttribute($value)
     {
-        $this ->attributes['expired'] = date('Y-m-t', strtotime($value));
+        $this->attributes['expired'] = date('Y-m-t', strtotime($value));
     }
 
-public function Camp():BelongsTo
-{
-    return $this->belongsTo(Camp::class);
-}
-public function User():BelongsTo
-{
-    return $this->belongsTo(User::class);
-}
+    /**
+     * Get the Camp that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function Camp(): BelongsTo
+    {
+        return $this->belongsTo(Camp::class);
+    }
 
+    /**
+     * Get the User that owns the Checkout
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function User(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
